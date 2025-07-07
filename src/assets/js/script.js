6bb7822ada6c7397129b4e45ca8a2ca9d7898b7e
Henrique Schell <henrique.schell@gmail.com>
2025-07-07 20:28:43 -0300

    projeto easybank

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,13 +1,15 @@
 var menuBtn = document.getElementById('menuBtn')
 var menu = document.getElementById('menu')
-var body = document.getElementById('body')
+var overlay = document.getElementById('overlay')
 
 menuBtn.addEventListener('click', () => {
     if(menu.style.display == 'none'){
-        menu.style.display = 'block'
+        menu.style.display = 'flex'
         menuBtn.src = 'src/images/icon-close.svg'
+        overlay.style.display = 'flex'
     }else{
         menu.style.display = 'none'
         menuBtn.src = 'src/images/icon-hamburger.svg'
+        overlay.style.display = 'none'
     }
 })
